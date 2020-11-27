4b962e63fc57816937906ec9f923e493d0a750d0
E.DIGBEU <digbeumichel77@gmail.com>
2020-11-27 14:27:23 +0000

    VUE ACTION NEW TICKET

---
+++ web-api/Services/ActionTicketService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using web_api.Models;
+using web_api.Models.Dto.Commercial;
+using web_api.Validator.Commercial.Ticket;
+
+namespace web_api.Services
+{
+    public class ActionTicketService : ServiceBase
+    {
+        public ActionTicketService(NovarelContext context) : base(context) { }
+
+        public async Task<ActionTicket> AddNewAction(ActionTicketValidator data)
+        {
+            var ticket = _context.Tickets.Where(t => t.Reference == data.reference).FirstOrDefault();
+            if(ticket.Id != 0)
+            {
+                var action = new ActionTicket();
+                action.Commentaire = data.commentaire;
+                action.DateAction = DateTime.Now;
+                action.Ticket = ticket;
+                await _context.ActionTickets.AddAsync(action);
+                _context.SaveChanges();
+                return action;
+            }
+            return null;
+        }
+    }
+}
